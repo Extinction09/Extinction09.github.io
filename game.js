cd795eae57bacccb7556e9d8c86629ca8a0ebb5b
Extinction09 <extinctionfr@gmail.com>
2018-12-14 18:26:58 -0500

    new build

--- game.js
+++ game.js
@@ -1,92 +1,137 @@
-//country stats
-var provinces = 1;
-var manpower = 0;
-var income = 0;
-var totalMoney = 57;
-var totalDevelopment = 0;
-var population = 0;
+var player = {
 
-var totalEarthProvinces = 36524;
 
-//province stats
-var provTypeTax = 1;
-var provCost = 0;
-var provTaxIncome = 0.1;
-var provTypeManpower = 1;
-var provManpowerIncome = 0.1;
-var provTypeProduction = 1;
-var developmentCost = Math.floor(10 * Math.pow(1.1, totalDevelopment));
-//province stats - buildings
-var temple = 0;
-var templeCost = 100;
-var templeIncome = 0.1;
-var totalTempleIncome = 0;
-var barracks = 0;
-var barracksCost = 140;
-var barracksIncrease = 0.5;
-var regimentQuarters = 0;
-var rQuartersCost = 150;
-var rQuartersIncrease = 0.25;
-var buildingSlots = 0;
+    //country stats
+        provinces: 1,
+        manpower: 0,
+        income: 0,
+        totalMoney: 700,
+        totalMoneyEarned: 0,
+        population: 0,
+        
+    //province stats
+        provTypeTax: 1,
+        provTypeManpower: 1,
+        provManpowerIncome: 0.1,
+        provTypeProduction: 1,
+        developmentCost: 10,
+    //province stats - buildings
+        buildingSlots: 0,
+    //taxation
+        temple: 0,
+        templeCost: 100,
+        templeIncome: 0.1,
+    //production
+        workshop: 0,
+        workshopCost: 100,
+    //manpower
+        barracks: 0,
+        barracksCost: 140,
+        barracksIncrease: 0.5,
+    //amount of regiments
+        regimentQuarters: 0,
+        rQuartersCost: 150,
+        rQuartersIncrease: 0.25,
+    
+    
+    //army stats
+        totalRegiments: 0,
+        regimentUK: 0,
+        morale: 1,
+        discipline: 0.5,
+        infantryAmount: 0,
+        infantryDamage: 1,
+        infantryCost: 11,
+        infantryUK: 0.2,
+        infantryHealth: 1,
+        cavalryAmount: 0,
+        cavalryCost: 25,
+        cavalryDamage: 1.35,
+        cavalryUK: 0.5,
+        cavalryHealth: 0.7,
+        cannonAmount: 0,
+        cannonDamage: 2,
+        cannonCost: 52,
+        cannonUK: 0.7,
+        cannonHealth: 0.5,
+    //army stats - manpower
+        manpowerCap: 4,
+        manpowerOL: 0,
+    //enemy stats
+        totalEnemyAttack: 7,
+    };
+    
+    var defaultPlayer = player;
+    var totalEarthProvinces = 36524;
+    var totalMaxAttack = Math.floor((player.cavalryDamage * player.cavalryAmount) + (player.cannonDamage * player.cannonAmount) + (player.infantryDamage * player.infantryAmount) * 6);
+    var totalMinAttack = Math.floor((player.cavalryDamage * player.cavalryAmount) + (player.cannonDamage * player.cannonAmount) + (player.infantryDamage * player.infantryAmount) * 1);
+    var totalHealth = Math.floor((player.cavalryHealth * player.cavalryAmount) + (player.cannonHealth * player.cannonAmount) + (player.infantryHealth * player.infantryAmount));
+    
 
-//army stats
-var totalRegiments = 0;
-var regimentUK = 0;
-var morale = 1;
-var discipline = 0.5;
-var infantryAmount = 0;
-var infantryDamage = 1;
-var infantryCost = 11;
-var infantryUK = 0.2;
-var infantryHealth = 1;
-var cavalryAmount = 0;
-var cavalryCost = 25;
-var cavalryDamage = 1.35;
-var cavalryUK = 0.5;
-var cavalryHealth = 0.7;
-var cannonAmount = 0;
-var cannonDamage = 2;
-var cannonCost = 52;
-var cannonUK = 0.7;
-var cannonHealth = 0.5; 
-
-var totalMaxAttack = Math.floor((cavalryDamage * cavalryAmount) + (cannonDamage * cannonAmount) + (infantryDamage * infantryAmount) * 6);
-var totalMinAttack = Math.floor((cavalryDamage * cavalryAmount) + (cannonDamage * cannonAmount) + (infantryDamage * infantryAmount) * 1);
-var totalHealth = Math.floor((cavalryHealth * cavalryAmount) + (cannonHealth * cannonAmount) + (infantryHealth * infantryAmount));
-
-//army stats - manpower
-var manpowerCap = 0;
-var manpowerOL = 0;
-
-//enemy stats
-var totalEnemyAttack = 7;
-
+    function set_cookie(cookie_name,value) {
+        expiry = new Date();   
+        expiry.setTime(new Date().getTime() + (365*24*60*60*1000)); 
+        var c_value=escape(btoa(JSON.stringify(value))) + 
+        "; expires="+expiry.toUTCString();
+        document.cookie=cookie_name + "=" + c_value;
+    }
+    
+    function get_cookie(cookie_name) {
+        var c_value = document.cookie;
+        var c_start = c_value.indexOf(" " + cookie_name + "=");
+        if (c_start == -1) {
+            c_start = c_value.indexOf(cookie_name + "=");
+        }
+        if (c_start == -1) return false;
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = atob(unescape(c_value.substring(c_start,c_end)));
+        return JSON.parse(c_value);
+    }
+    
+    function load_game() {
+        var save_data = get_cookie('save');
+           if (!save_data) return;
+      player = save_data;
+      }
+      function save_game() {
+        set_cookie('save', player);
+    }
 
 
 //html stuff
 function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-  
-    // Remove the background color of all tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].style.backgroundColor = "";
     }
+        document.getElementById(pageName).style.display = "block";
+        elmnt.style.backgroundColor = color;
+}
+document.getElementById("defaultOpen").click();
+
+
+
+//help menu
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
   
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
+
+
+
+
 
 
 //rng
@@ -97,33 +142,22 @@ function randomGet() {
 }
 
 //random country name
-function generate(){
+function generate() {
 	var adjectives = ["Revolutionary","Greater","Northern","North","Southern","Western","Eastern","Higher","Polluted","Ruined","The Empire of","Ancient","","Racist","Humanist",
     "Militaristic","Rebellious","Sanctioned","Bankrupt","Exiled","Excommunicated","Upside Down","Right Side Up","Crooked","Gargantuan","Obese","Lower","The Island Nation of",
     "Cocaine Filled","Weed Loaded","Inhumane","Underwater","Heavenly","Hellish","Embargoed","Shooketh","Starved","Famine Filled","Casualtyville","One Province Minor",
-    "The Duchy of","The Kingdom of","Refugeeville","Nuclear Plant Filled","Massive CO2 Generator","Hypocritcal","A Failure Named"];
+    "The Duchy of","The Kingdom of","Refugeeville","Nuclear Plant Filled","Massive CO2 Generator","Hypocritcal","A Failure Named","Sultanate of","Daimyo of","Imperial","Terrible",
+    "Toasty","Angelic"];
 	var nouns= ["Austria","Australia","France","England","Wales","Mexico","Spain","Portugal","Germany","Prussia","Italy","Pope Boi","Switzerland","Denmark","Ireland","Scotland",
     "Poland","Muscovy","Sweden","Morocco","Tunis","Byzantium","Hungary","Brandenburg","Norway","Korea","Greece","Ming","US","Canada","Japan","Britain","Netherlands","Rome",
     "Russia","Persia","Holland","Egypt","Novgorod","Ethiopia","New Zealand","Philippines","T-Series","FYROM","Estonia","Latvia","Lithuania","Ukraine","China","Vietnam",
-    "Mongolia","Scandinavia","Greenland","Brazil","Union of Soviet Socialist Republics","German Reich","Nationalist France","East Germany","Cuba","Austria-Hungary"];
+    "Mongolia","Scandinavia","Greenland","Brazil","Union of Soviet Socialist Republics","German Reich","Nationalist France","East Germany","Cuba","Austria-Hungary","Czechia","Slovakia","Belarus","Qing",
+    "Manchu","Ashikaga","Obama","Algeria","Bolivia","Zimbabwe","Nicagaragua","Macedon"];
 	var rand_adjectives = Math.floor(Math.random()*adjectives.length); 
 	var rand_nouns = Math.floor(Math.random()*nouns.length); 
-	document.getElementById('result').innerHTML = "<div class='alert alert-success'><h2>"+adjectives[rand_adjectives]+" "+nouns[rand_nouns]+"</h2></div>";
- 
+    document.getElementById('countryName').innerHTML = adjectives[rand_adjectives]+" "+nouns[rand_nouns];
+    document.getElementById('title').innerHTML = adjectives[rand_adjectives]+" "+nouns[rand_nouns];
 }
-
-
-//total development function
-function devTotal() {
-    totalDevelopment = provTypeTax + provTypeManpower + provTypeProduction;
-    document.getElementById('totalDev').innerHTML = totalDevelopment;
-};
-
-//province total function
-function provTotal() {
-    totalProv = provinces;
-    document.getElementById('totalProv').innerHTML = totalProv;
-};
 
 
 
@@ -131,19 +165,23 @@ function provTotal() {
 
 //buy province
 function buyProvince() {
-    if (totalMoney >= provCost) {
-        province = province + 1;
-        provTypeTax = proveTypeTax * getRandom;
-        provTypeProduction = provTypeProduction * getRandom;
-        provTypeManpower = provTypeProduction * getRandom;
-        document.getElementById('provinces').innerHTML = province;
-        document.getElementById('pTax').innerHTML = provTypeTax;
-        document.getElementById('pProduction').innerHTML = provTypeProduction;
-        document.getElementById('pManpower').innerHTML = provTypeManpower;
+    var provCost = Math.floor(100 * Math.pow(1.10, (player.provinces - 1)));
+    if (player.totalMoney >= provCost) {
+        player.totalMoney = player.totalMoney - provCost;
+        player.provinces++;
+        player.provTypeTax = player.provTypeTax + Math.round(Math.random() * 6 - 0);
+        player.provTypeProduction = player.provTypeProduction + Math.round(Math.random() * 3 - 0);
+        player.provTypeManpower = player.provTypeProduction + Math.round(Math.random() * 4 - 0);
+        document.getElementById('provinces').innerHTML = player.provinces;
+        document.getElementById('pTax').innerHTML = player.provTypeTax;
+        document.getElementById('pProduction').innerHTML = player.provTypeProduction;
+        document.getElementById('pManpower').innerHTML = player.provTypeManpower;
+        document.getElementById('totalDev').innerHTML = totalDevelopment;
+        document.getElementById('income').innerHTML = player.income;
     } else {
         return;
     };
-    var nextCost = Math.floor(50 * Math.pow(1.10, provinces));
+    var nextCost = Math.floor(100 * Math.pow(1.10, (player.provinces - 1)));
     document.getElementById('provCost').innerHTML = nextCost;
 };
 
@@ -153,65 +191,101 @@ function buyProvince() {
 
 
 
-
-//function of provTypeManpowerIncome
-function provTypeMI() {
-    if (manpower >= manpowerCap) {
-    return;
-    } else {
-        manpower = manpower + provManpowerIncome;
-    };
+//calculate building slots per province
+function getSlots() {
+    player.buildingSlots = player.provinces * 20;
+    document.getElementById('totalSlots').innerHTML = player.buildingSlots;
 };
 
-//develop provTypeManpower
-function devProvTypeManpower() {
-    if(totalMoney >= developmentCost) {
-        totalMoney - developmentCost;
-        provTypeManpower + 1;
-        document.getElementById('pManpower').innerHTML = provTypeManpower;
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.10, totalDevelopment));
-    document.getElementById('devCost').innerHTML = nextCost;
+function getDev() {
+    var totalDevelopment = player.provTypeManpower + player.provTypeTax + player.provTypeProduction;
+    document.getElementById('totalDev').innerHTML = totalDevelopment;
 };
 
-
-
-
-
-//function of provTypeTax
-function provinceTI() {
-    provTaxIncome + totalMoney;
+function getMoney() {
+    document.getElementById('totalMoney').innerHTML = player.totalMoney.toFixed(1);
 };
+
+function getIncome() {
+    var totalIncome  
+};
+
 
 //develop provTypeTax
 function devProvTypeTax() {
-    if(totalMoney >= developmentCost) {
-        totalMoney - developmentCost;
-        provTypeTax + 1;
-        document.getElementById('pTax').innerHTML = provTypeTax;
+    if(player.totalMoney >= player.developmentCost) {
+        player.totalMoney = player.totalMoney - player.developmentCost;
+        player.provTypeTax++;
+        totalDevelopment++;
+        document.getElementById('pTax').innerHTML = player.provTypeTax;
+        document.getElementById('totalDev').innerHTML = totalDevelopment;
+        document.getElementById('totalMoney').innerHTML = player.totalMoney;
     };
     var nextCost = Math.floor(10 * Math.pow(1.10, totalDevelopment));
     document.getElementById('devCost').innerHTML = nextCost;
 };
 
-
-
-
-
-//function of provTypeProduction
-function provincePI() {
-    provProdIncome + totalMoney;
+//function of provTypeTax
+function provTypeTaxIncome() {
+    var provTaxIncome = 0.1;
+    provTaxIncome = Math.round(provTaxIncome * player.provTypeTax);
+    player.totalMoney = player.totalMoney + provTaxIncome;
+    document.getElementById('totalMoney').innerHTML = player.totalMoney;
 };
+
+
+
+
 
 //develop provTypeProduction
 function devProvTypeProduction() {
-    if(totalMoney >= developmentCost) {
-        totalMoney - developmentCost;
-        provTypeProduction + 1;
-        document.getElementById('pProduction').innerHTML = provTypeProduction;
+    if(player.totalMoney >= player.developmentCost) {
+        player.totalMoney = player.totalMoney - player.developmentCost;
+        player.provTypeProduction++;
+        totalDevelopment++;
+        document.getElementById('pProduction').innerHTML = player.provTypeProduction;
+        document.getElementById('totalDev').innerHTML = totalDevelopment;
+        document.getElementById('totalMoney').innerHTML = player.totalMoney;
     };
     var nextCost = Math.floor(10 * Math.pow(1.10, totalDevelopment));
     document.getElementById('devCost').innerHTML = nextCost;
+};
+
+//function of provTypeProduction
+function provTypeProductionIncome() {
+    var provProductionIncome = 0.1;
+    provProductionIncome = provProductionIncome * player.provTypeProduction;
+    player.totalMoney = player.totalMoney + provProductionIncome;
+};
+
+
+
+
+
+//develop provTypeManpower
+function devProvTypeManpower() {
+    if(player.totalMoney >= player.developmentCost) {
+        player.totalMoney = player.totalMoney - player.developmentCost;
+        player.provTypeManpower++;
+        totalDevelopment++;
+        document.getElementById('pManpower').innerHTML = player.provTypeManpower;
+        document.getElementById('totalDev').innerHTML = totalDevelopment;
+        document.getElementById('totalMoney').innerHTML = player.totalMoney;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.10, totalDevelopment));
+    document.getElementById('devCost').innerHTML = nextCost;
+};
+
+//function of provTypeManpowerIncome
+function provTypeManpowerIncome() {
+    if (player.manpower >= player.manpowerCap) {
+        player.manpower = player.manpower - player.manpower;
+        player.manpower = player.manpower + player.manpowerCap;
+        document.getElementById('manpower').innerHTML = player.manpower.toFixed(1);
+        return;
+    } else {
+        player.manpower = player.manpower + player.provManpowerIncome;
+    };
 };
 
 
@@ -220,17 +294,43 @@ function devProvTypeProduction() {
 
 //purchase temple
 function buyTemple() {
-    if(totalMoney >= templeCost) {
-        totalMoney - templeCost;
-        temple + 1;
-        document.getElementById('temple').innerHTML = temple;
+    if(player.totalMoney >= player.templeCost) {
+        player.totalMoney = player.totalMoney - player.templeCost;
+        player.temple++;
+        document.getElementById('temple').innerHTML = player.temple;
+        document.getElementById('totalMoney').innerHTML = player.totalMoney;
+        document.getElementById('templeIncome').innerHTML = templeIncome.toFixed(1);
     };
 };
 
 //function of temple
 function templeIncrease() {
-    totalTempleIncome = temple * templeIncome;
-    totalMoney = totalMoney + totalTempleIncome;
+    var templeIncome = 0.1 * player.provTypeTax;
+    templeIncome = templeIncome * player.temple;
+    player.totalMoney = player.totalMoney + templeIncome;
+    document.getElementById('totalMoney').innerHTML = player.totalMoney;
+    document.getElementById('templeIncome').innerHTML = templeIncome.toFixed(1);
+    document.getElementById('income').innerHTML = player.income;
+};
+
+//purchase workshop
+function buyWorkshop() {
+    if(player.totalMoney >= player.workshopCost) {
+        player.totalMoney = player.totalMoney - player.workshopCost;
+        player.workshop = player.workshop + 1;
+        document.getElementById('workshop').innerHTML = player.workshop;
+        document.getElementById('totalMoney').innerHTML = player.totalMoney;
+    };
+};
+
+//function of workshop
+function workshopIncrease() {
+    var workshopIncome = 0.1 * player.provTypeProduction;
+    workshopIncome = workshopIncome * player.workshop;
+    player.totalMoney = player.totalMoney + workshopIncome;
+    document.getElementById('totalMoney').innerHTML = player.totalMoney;
+    document.getElementById('workshopIncome').innerHTML = workshopIncome.toFixed(1);
+    document.getElementById('income').innerHTML = player.income;
 };
 
 
@@ -239,25 +339,25 @@ function templeIncrease() {
 
 //purchase infantry
 function purchaseInfantry() {
-    if(totalMoney >= infantryCost) {
-        infantry = infantry + 1;
-        totalMoney = totalMoney - infantryCost;
+    if(player.totalMoney >= player.infantryCost) {
+        player.infantry++;
+        player.totalMoney = player.totalMoney - player.infantryCost;
     };
 };
 
 //purchase cavalry
 function purchaseCavalry() {
-    if(totalMoney >= cavalryCost) {
-        cavalryAmount = cavalryAmount + 1;
-        totalMoney = totalMoney - cavalryCost;
+    if(player.totalMoney >= player.cavalryCost) {
+        player.cavalryAmount++;
+        player.totalMoney = player.totalMoney - player.cavalryCost;
     };
 };
 
 //purchase cannon
 function purchaseCannon() {
-    if(totalMoney >= cannonCost) {
-        cannonAmount = cannonAmount + 1;
-        totalMoney = totalMoney - cannonCost;
+    if(player.totalMoney >= player.cannonCost) {
+        player.cannonAmount = player.cannonAmount++;
+        player.totalMoney = player.totalMoney - player.cannonCost;
     };
 };
 
@@ -265,8 +365,22 @@ function purchaseCannon() {
 
 
 
-window.setInterval(function(){
+//achievements
 
-    provinceTI(provTypeTax);
+
+
+
+window.setInterval(function(){
+    getDev();
+    getSlots();
+    getMoney();
+}, 50);
+
+
+window.setInterval(function(){
+    provTypeTaxIncome(player.provTypeTax);
+    provTypeManpowerIncome(player.provTypeManpower);
+    provTypeProductionIncome(player.provTypeProduction);
+    workshopIncrease(workshop);
     templeIncrease(temple);
 }, 1000);
